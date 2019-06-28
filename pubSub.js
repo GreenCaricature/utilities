3e8a9bc187f5dd63821b67b00e6f794de5a83e89
Seth Todd <sethltodd@gmail.com>
2019-06-28 12:16:58 -0700

    adding unpublish method, updating logic

--- pubSub.js
+++ pubSub.js
@@ -1,28 +1,54 @@
-var pubSub = window["pubSub"] || {
+var pubSub = window.utag["tealiumPubSub"] || {
   events : {},
-  //whee, magic number
-  token_iterator : 0,
-  // returns a token that identifies this particular subscription to be used for unsubscribing
+  // magic number as baseline for subscription IDs
+  subscription_id_iterator : 0,
   subscribe : function (event, callback) {
-    var token = pubSub.token_iterator++;
-    pubSub.events[event] = pubSub.events[event] || [];
-    pubSub.events[event].push({callback : callback, token : token});
-    return token;
+    var subscription_id;
+    pubSub.events[event] = pubSub.events[event] || {subscriptions:[],has_published:false};
+    if (pubSub.events[event].has_published) {
+      callback();
+    } else {
+      subscription_id = utag.tealiumPubSub.subscription_id_iterator++;
+      pubSub.events[event].subscriptions.push({callback : callback, subscription_id : subscription_id});
+    }
+    return subscription_id;
   },
   publish : function (event) {
     var subscription;
-    pubSub.events[event] = pubSub.events[event] || [];
-    for (subscription in pubSub.events[event]) {
-      pubSub.events[event][subscription].callback();
+    pubSub.events[event] = pubSub.events[event] || {subscriptions:[],has_published:false};
+    if (!utag.tealiumPubSub.events[event].has_published) {
+      pubSub.events[event].has_published = true;
+      // if there are pre-existing subscriptions, invoke their callbacks
+      if (pubSub.events[event].subscriptions.length > 0) {
+        for (subscription in pubSub.events[event].subscriptions) {
+          pubSub.events[event].subscriptions[subscription].callback();
+        } 
+        // clears the original array after subscriptions have been triggered
+        pubSub.events[event].subscriptions.length = 0;
+      }    
     }
   },
-  unsubscribe : function (token) {
-    var subscription, i;
-    for (subscription in pubSub.events) {
-      for (i = 0; i < pubSub.events[subscription].length; i++) {
-        //removes the subscription by its token
-        if (token === pubSub.events[subscription][i].token) {
-          pubSub.events[subscription].splice(i,1);
+  unsubscribe : function (subscription_id) {
+    var entry, i;
+    for (entry in pubSub.events) {
+      if (pubSub.events.hasOwnProperty(entry)) {
+        for (i = 0; i < utag.tealiumPubSub.events[entry].subscriptions.length; i++) {
+          //removes the subscription by its id
+          if (subscription_id === pubSub.events[entry].subscriptions[i].subscription_id) {
+            pubSub.events[entry].subscriptions.splice(i,1);
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  },
+  unpublish : function (event) {
+    var entry;
+    for (entry in pubSub.events) {
+      if (pubSub.events.hasOwnProperty(entry)) {
+        if (event === entry) {
+          pubSub.events[entry].has_published = false;
           return true;
         }
       }
