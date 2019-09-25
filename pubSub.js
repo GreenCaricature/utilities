6370d117ff2ad783c30760bab208c2513c478762
Seth Todd <sethltodd@gmail.com>
2019-09-25 16:01:05 -0700

    update loops to use let; replace magic number with global Symbol

--- pubSub.js
+++ pubSub.js
@@ -1,26 +1,22 @@
 var pubSub = window.pubSub || {
   events : {},
-  // magic number as baseline for subscription IDs
-  subscription_id_iterator : 0,
   subscribe : function (event, callback) {
-    var subscription_id;
+    const subscription_id = Symbol.for(event);
     pubSub.events[event] = pubSub.events[event] || {subscriptions:[],has_published:false};
     if (pubSub.events[event].has_published) {
       callback();
     } else {
-      subscription_id = pubSub.subscription_id_iterator++;
       pubSub.events[event].subscriptions.push({callback : callback, subscription_id : subscription_id});
     }
     return subscription_id;
   },
   publish : function (event) {
-    var subscription;
     pubSub.events[event] = pubSub.events[event] || {subscriptions:[],has_published:false};
     if (!pubSub.events[event].has_published) {
       pubSub.events[event].has_published = true;
       // if there are pre-existing subscriptions, invoke their callbacks
       if (pubSub.events[event].subscriptions.length > 0) {
-        for (subscription in pubSub.events[event].subscriptions) {
+        for (let subscription in pubSub.events[event].subscriptions) {
           pubSub.events[event].subscriptions[subscription].callback();
         } 
         // clears the original array after subscriptions have been triggered
@@ -29,10 +25,9 @@ var pubSub = window.pubSub || {
     }
   },
   unsubscribe : function (subscription_id) {
-    var entry, i;
-    for (entry in pubSub.events) {
+    for (let entry in pubSub.events) {
       if (pubSub.events.hasOwnProperty(entry)) {
-        for (i = 0; i < pubSub.events[entry].subscriptions.length; i++) {
+        for (let i = 0; i < pubSub.events[entry].subscriptions.length; i++) {
           //removes the subscription by its id
           if (subscription_id === pubSub.events[entry].subscriptions[i].subscription_id) {
             pubSub.events[entry].subscriptions.splice(i,1);
@@ -44,8 +39,7 @@ var pubSub = window.pubSub || {
     return false;
   },
   unpublish : function (event) {
-    var entry;
-    for (entry in pubSub.events) {
+    for (let entry in pubSub.events) {
       if (pubSub.events.hasOwnProperty(entry)) {
         if (event === entry) {
           pubSub.events[entry].has_published = false;
